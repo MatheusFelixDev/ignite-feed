c34fda854d0196d27785bfc44240d4204ab5eca5
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2024-01-07 22:46:36 -0300

    add component avatar and responsive

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,12 @@
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+        <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { PencilLine } from 'phosphor-react';
-
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -11,7 +11,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/MatheusFelixDev.png" />
+      <Avatar src="https://github.com/maykbrito.png" />
 
         <strong>Matheus Felix</strong>
         <span>Web Developer</span>
